10cf6dd8159d25aa86a96200d3a6b7bdd99cd49e
jacobg <gilletja@oregonstate.edu>
2021-03-07 11:28:46 -0800

    Created fizzbuzz file to determine if input number is divisible by 3 or 5 if the number is within the range 1-100

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+import unittest
+
+def fun(x):
+    if x < 1 or x > 100:
+        return "Invalid Input"
+    if x % 3 == 0:
+        if x % 5 == 0:
+            return "FizzBuzz"
+        else:
+            return "Fizz"
+    elif x % 5 == 0:
+        return "Buzz"
