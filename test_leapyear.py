2c537a0e54109b949f93acf24d3fba2c4850730f
jacobg <gilletja@oregonstate.edu>
2021-03-07 11:49:18 -0800

    added test cases to ensure that all branches of code are being tested

--- test_leapyear.py
+++ test_leapyear.py
@@ -7,8 +7,13 @@ class TestCase(unittest.TestCase):
         self.assertEqual(leapyear.fun(0), "Invalid Input") #should throw an error as it's out of the range of numbers
 
     def test_leapyear1(self):
-        self.assertEqual(leapyear.fun(4), "Leap year") #should throw an error as it's out of the range of numbers
+        self.assertEqual(leapyear.fun(4), "Input is a leap year") #Return as a leap year
 
+    def test_leapyear2(self):
+        self.assertEqual(leapyear.fun(100), "Input is not a leap year") #Return as not a leap year
+
+    def test_leapyear3(self):
+        self.assertEqual(leapyear.fun(400), "Input is a leap year") #Return as a leap year
 
 if __name__ == '__main__':
   unittest.main(verbosity=2)
