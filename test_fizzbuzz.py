1ad7f39dd0170a1d17ce67b11273aa09d74ec5e4
jacobg <gilletja@oregonstate.edu>
2021-03-07 11:50:27 -0800

    fixed comments

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -11,13 +11,13 @@ class TestCase(unittest.TestCase):
         self.assertEqual(fizzbuzz.fun(15), "FizzBuzz") #Should pass the test as 15 is divisible by 3 and 5
 
     def test_fizzbuzz2(self):
-        self.assertEqual(fizzbuzz.fun(98), 98) #Should pass the test as 15 is divisible by 3 and 5
+        self.assertEqual(fizzbuzz.fun(98), 98) #Should pass the test and return 98 as 98 is not divisible by 3 or 5
 
     def test_fizzbuzz3(self):
-        self.assertEqual(fizzbuzz.fun(6), "Fizz") #Should pass the test as 15 is divisible by 3 and 5
+        self.assertEqual(fizzbuzz.fun(6), "Fizz") #Should pass the test as 6 is divisible by 3
 
     def test_fizzbuzz4(self):
-        self.assertEqual(fizzbuzz.fun(10), "Buzz") #Should pass the test as 15 is divisible by 3 and 5
+        self.assertEqual(fizzbuzz.fun(10), "Buzz") #Should pass the test as 10 is divisible by 5
 
 if __name__ == '__main__':
   unittest.main(verbosity=2)
