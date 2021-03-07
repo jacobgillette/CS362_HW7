e2c4c333f8d264bf0d01ad5f7f948fd2780aff71
jacobg <gilletja@oregonstate.edu>
2021-03-07 11:34:46 -0800

    added more test cases

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -4,11 +4,20 @@ import fizzbuzz
 
 class TestCase(unittest.TestCase):
 
-  def test_fizzbuzz(self):
-    self.assertEqual(fizzbuzz.fun(0), 0) #should throw an error as it's out of the range of numbers
+    def test_fizzbuzz(self):
+        self.assertEqual(fizzbuzz.fun(0), "Invalid Input") #should throw an error as it's out of the range of numbers
 
-  def test_fizzbuzz1(self):
-    self.assertEqual(fizzbuzz.fun(15), fizzbuzz) #Should pass the test as 15 is divisible by 3 and 5
+    def test_fizzbuzz1(self):
+        self.assertEqual(fizzbuzz.fun(15), "FizzBuzz") #Should pass the test as 15 is divisible by 3 and 5
+
+    def test_fizzbuzz2(self):
+        self.assertEqual(fizzbuzz.fun(98), 98) #Should pass the test as 15 is divisible by 3 and 5
+
+    def test_fizzbuzz3(self):
+        self.assertEqual(fizzbuzz.fun(6), "Fizz") #Should pass the test as 15 is divisible by 3 and 5
+
+    def test_fizzbuzz4(self):
+        self.assertEqual(fizzbuzz.fun(10), "Buzz") #Should pass the test as 15 is divisible by 3 and 5
 
 if __name__ == '__main__':
   unittest.main(verbosity=2)
